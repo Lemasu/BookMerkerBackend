1a95e840d8dc550260d4f4dbc50f6e290db083f3
Lemasu <alex.schaub@lernende.bbw.ch>
2022-07-15 09:44:03 +0200

    Backend soll mit Java Spring implementiert werden
    Das Backend soll mithilfe von Java Spring implementiert werden.

---
+++ src/main/java/ch/bbw/as/bookmerkerbackend/BookMerkerBackendApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.as.bookmerkerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMerkerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMerkerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/bbw/as/bookmerkerbackend/BookMerkerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.as.bookmerkerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMerkerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
